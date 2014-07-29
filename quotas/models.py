e363d87e56e8a8d860697a456dc0016fad3b7a89
e-shulitsky <e.shulitsky@itransition.com>
2014-07-29 13:14:31 +0300

    BONGOMAGIC-1261 Custom User model preparation

--- quotas/models.py
+++ quotas/models.py
@@ -1,36 +1,40 @@
 from django.conf import settings
-from django.db import models
-from django.contrib.auth.models import User
-
+from django.contrib.auth import get_user_model
 from quotas import UNLIMITED
 
 QUOTAS = getattr(settings, 'QUOTAS', {})
+
 
 def _user_get_quota(user, base):
     """Return user's quota for `base'.
 
     Returns True if unlimited, integer if limited, None if no quota was set."""
     def __has_quota_of(limit):
-        return user.has_perm( 'quotas.quota_%s_%s'%(base, limit) )
-    if __has_quota_of('unlimited'): return UNLIMITED
+        return user.has_perm('quotas.quota_%s_%s' % (base, limit))
+    if __has_quota_of('unlimited'):
+        return UNLIMITED
     try:
-        for v in sorted( QUOTAS[base], reverse=True ):
+        for v in sorted(QUOTAS[base], reverse=True):
             if __has_quota_of(v):
                 return v
-    except KeyError: pass               # no base quota
+    except KeyError:
+        pass               # no base quota
     return None
 # User.add_to_class('get_quota', _user_get_quota)
+
 
 class UserQuotas(object):
     def __init__(self, user):
         self.__user = user
+
     def __getattr__(self, quota_name):
         if quota_name in QUOTAS:
             return _user_get_quota(self.__user, quota_name)
         raise AttributeError
 
+
 def _user_get_quotas(user):
     return UserQuotas(user)
 
-User.add_to_class('quotas', property(_user_get_quotas))
+get_user_model().add_to_class('quotas', property(_user_get_quotas))
 
